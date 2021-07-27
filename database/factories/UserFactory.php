74409e5bd49c358732e09cd5c7cb627f97d05e60
Jorge Renteral <jorgerenteral@gmail.com>
2021-07-27 12:35:54 -0500

    Factory arreglado

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'fecha_nacimento' => date('Y-m-d', strtotime('-' . rand(18, 25) . ' years and -' . rand(1, 31) . ' days'))
+            'fecha_nacimento' => $this->faker->dateTimeBetween('-70 years', '-18 years')
         ];
     }
 
